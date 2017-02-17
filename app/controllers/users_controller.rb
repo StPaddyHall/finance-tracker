6110371dea23c51233f999f9e72f60da3bf47ae2
Patrick <artificial.kulture@gmail.com>
2017-02-17 22:22:46 +0000

    add user and stock association and display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
    def my_portfolio
-       
+      # gets the many to many association
+      @user_stocks = current_user.stocks
+      @user = current_user
    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,26 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :user_stocks
+  has_many :stocks, through: :user_stocks
+  
+  def can_add_stock?(ticker_symbol)
+    # calls the under stock limit and the stock already added methods
+    under_stock_limit? && !stock_already_added?(ticker_symbol)
+    
+  end
+  
+  # method to check user hasn't reached thier limit of 10 stocks
+  def under_stock_limit?
+    (user_stocks.count < 10)
+  end
+  
+  # method to check user hasn't already added their stock to their portfolio
+  def stock_already_added?(ticker_symbol)
+    stock = Stock.find_by_ticker(ticker_symbol)
+    return false unless stock
+    
+    # checks the db to determine if that user has the stock id assigned to them already
+    user_stocks.where(stock_id: stock.id).exists?
+  end
 end
